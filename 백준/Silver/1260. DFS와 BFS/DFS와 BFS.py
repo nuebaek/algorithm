f8fde31c0bfe4e371f7595ea1c978ba77b08e6c7
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2025-10-24 14:23:54 +0900

    [Silver II] Title: DFS와 BFS, Time: 324 ms, Memory: 35440 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,32 +1,77 @@
+# 인접 행렬
+# from collections import deque
+#
+# n, m, v = map(int, input().split())
+# graph = [[0]*(n+1) for _ in range(n+1)]
+# for i in range(m):
+#     x, y = map(int, input().split())
+#     graph[x][y] = graph[y][x] = 1
+# def dfs(graph, v, visited):
+#     visited[v] = True
+#     print(v, end=" ")
+#
+#     for i in range(1, n+1):
+#         if graph[v][i] ==1 and not visited[i]:
+#             dfs(graph, i, visited)
+#
+# def bfs(graph, v, visited):
+#     queue = deque([v])
+#     visited[v] = True
+#
+#     while queue:
+#         v = queue.popleft()
+#         print(v, end=" ")
+#         for i in range(1, n+1):
+#             if graph[v][i] == 1 and not visited[i]:
+#                 visited[i] = True
+#                 queue.append(i)
+#
+#
+# visited1 = [False] * (n+1)
+# visited2 = [False] * (n+1)
+#
+# dfs(graph, v, visited1)
+# print()
+# bfs(graph, v, visited2)
+
+
+# 인접 리스트
 from collections import deque
 
 n, m, v = map(int, input().split())
-
-graph = [[False]*(n+1) for _ in range(n+1)]
+graph = [[]*(n+1) for _ in range(n+1)]
 for i in range(m):
-    x, y = map(int, input().split())
-    graph[x][y] = graph[y][x] = 1
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-visited1 = [False]*(n+1)
-visited2 = [False]*(n+1)
+for i in range(1, n+1):
+    graph[i].sort()
 
-def dfs(v):
-    visited1[v] = True
+def dfs(graph, v, visited):
+    visited[v] = True
     print(v, end=" ")
-    for i in range(1, n+1):
-        if not visited1[i] and graph[v][i] == 1:
-            dfs(i)
-def bfs(v):
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+def bfs(graph, v, visited):
     queue = deque([v])
-    visited2[v] = True
+    visited[v] = True
+
     while queue:
         v = queue.popleft()
-        print(v, end= " ")
-        for i in range(1, n+1):
-            if not visited2[i] and graph[v][i]==1:
+        print(v, end=" ")
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
                 queue.append(i)
-                visited2[i] = True
 
-dfs(v)
+
+visited1 = [False] * (n+1)
+visited2 = [False] * (n+1)
+
+dfs(graph, v, visited1)
 print()
-bfs(v)
+bfs(graph, v, visited2)

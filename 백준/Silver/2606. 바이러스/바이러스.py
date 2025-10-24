ae8b061e6a4d12d179b233508bca82f30da2d768
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2025-10-24 15:39:43 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,20 +1,44 @@
+# 인접 행렬
+# n = int(input())
+# m = int(input())
+#
+# graph = [[0]*(n+1) for _ in range(n+1)]
+# for i in range(m):
+#     x, y = map(int, input().split())
+#     graph[x][y] = graph[y][x] = 1
+#
+# def dfs(graph, v, visited, n):
+#     visited[v] = True
+#     count = 1
+#
+#     for i in range(1, n + 1):
+#         if graph[v][i] == 1 and not visited[i]:
+#             count += dfs(graph, i, visited, n)
+#     return count
+#
+# visited = [False] * (n+1)
+# result = dfs(graph, 1, visited, n)
+# print(result-1)
+# print(graph)
+# print(visited)
+
+# 인접 리스트
 n = int(input())
 m = int(input())
 
-graph = [[0]*(n+1) for _ in range(n+1)]
+graph = [[]*(n+1) for _ in range(n+1)]
 for i in range(m):
-    x, y = map(int, input().split())
-    graph[x][y] = graph[y][x] = 1
-
-def dfs(graph, v, visited, n):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+def dfs(graph, v, visited):
     visited[v] = True
-    count = 1
 
-    for i in range(1, n + 1):
-        if graph[v][i] == 1 and not visited[i]:
-            count += dfs(graph, i, visited, n)
-    return count
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
 visited = [False] * (n+1)
-result = dfs(graph, 1, visited, n)
-print(result-1)
+
+dfs(graph, 1, visited)
+print(sum(visited)-1)

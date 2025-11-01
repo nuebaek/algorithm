28829b5b4ce8cd7c451c0e97aacf9bfbc94f5d80
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2025-11-01 23:56:34 +0900

    [Silver V] Title: 문서 검색, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -0,0 +1,15 @@
+import sys
+s = sys.stdin.readline().strip()
+x = sys.stdin.readline().strip()
+
+i = 0
+cnt = 0
+
+while i < len(s):
+    if s[i:i+len(x)] == x:
+        i += len(x)
+        cnt += 1
+    else :
+        i += 1
+        
+print(cnt)

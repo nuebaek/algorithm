5756e38df5d7dd1e3cb1f1d56bd9d27fcfd12617
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2025-12-16 23:48:35 +0900

    [Gold IV] Title: 전화번호 목록, Time: 120 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-t = int(input())
-for _ in range(t):
+def solve():
     n = int(input())
     lst = [input().strip() for _ in range(n)]
     lst.sort()
+    
     for i in range(n-1):
         if lst[i+1].startswith(lst[i]):
             print("NO")
-            break
-    else:
-        print("YES")
+            return  
+    print("YES")
+
+t = int(input())
+for _ in range(t):
+    solve() 

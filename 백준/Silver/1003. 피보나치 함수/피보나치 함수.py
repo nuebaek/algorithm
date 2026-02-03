cc79d5ef1d413433126f05f12ac203ab684b9b95
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-02-03 18:15:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+
+    zero = [1, 0] 
+    one = [0, 1] 
+
+    for i in range(2, n + 1):
+        zero.append(zero[i - 1] + zero[i - 2])
+        one.append(one[i - 1] + one[i - 2])
+
+    print(zero[n], one[n])

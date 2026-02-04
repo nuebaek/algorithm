00e5ebe3acb4f1c726ecafd025700de02aaa3b38
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-02-04 23:20:25 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 184 ms, Memory: 41144 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+prefix_sum = [0] * (n + 1)
+for i in range(n):
+    prefix_sum[i + 1] = prefix_sum[i] + a[i]
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(prefix_sum[e] - prefix_sum[s - 1])

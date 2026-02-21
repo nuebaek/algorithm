d2b5f0de527f43271239c2d754db7812108cc97d
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-02-21 23:28:28 +0900

    [Silver II] Title: N과 M (9), Time: 92 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+num = sorted(list(map(int, input().split())))
+
+visited = [0] * n
+answer = []
+
+def solve():
+    check = 0
+    if len(answer) == m:
+        print(*answer)
+        return
+    for i in range(n):
+        if check != num[i] and visited[i] == 0:
+            answer.append(num[i])
+            visited[i] = 1
+            check = num[i]
+            solve()
+            answer.pop()
+            visited[i] = 0
+
+solve()

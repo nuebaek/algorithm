c6e6159bb3c5d2222f9dc3b7f51bb5e396ae2cf6
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-03-07 13:11:56 +0900

    [Silver II] Title: 섬의 개수, Time: 60 ms, Memory: 32744 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,37 +1,35 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 dy = [-1, 1, 0, 0, -1, -1, 1, 1]
 dx = [0, 0, -1, 1, -1, 1, -1, 1]
 
-def bfs(start_y, start_x):
-    queue = deque([(start_y, start_x)])
-    graph[start_y][start_x] = 0 
+def dfs(start_x, start_y):
+    # if 0 <= i < h and 0 <= j < w :
+    if start_x < 0 or start_x >= h or start_y < 0 or start_y >= w or graph[start_x][start_y] != 1:
+        return
 
-    while queue:
-        y, x = queue.popleft()
+    graph[start_x][start_y] = 0
 
-        for i in range(8):
-            ny = y + dy[i]
-            nx = x + dx[i]
+    for i in range(8):
+        nx = start_x + dx[i]
+        ny = start_y + dy[i]
 
-            if 0 <= ny < h and 0 <= nx < w and graph[ny][nx] == 1:
-                graph[ny][nx] = 0  
-                queue.append((ny, nx))
+        dfs(nx, ny)
 
 while True:
     w, h = map(int, input().split())
     if w == 0 and h == 0:
         break
-        
+
     graph = [list(map(int, input().split())) for _ in range(h)]
     ans = 0
 
-    for i in range(h): 
-        for j in range(w): 
-            if graph[i][j] == 1: 
-                bfs(i, j)        
-                ans += 1        
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1:
+                dfs(i, j)
+                ans += 1
 
     print(ans)

69b7a68e5af23c51f007033c4ce464ba70f5c113
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-03-08 13:01:03 +0900

    [Gold V] Title: 1학년, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [[0]*21 for i in range(n-1)]
+dp[0][arr[0]] = 1
+
+for i in range(1, n-1):
+    for j in range(21):
+        if j-arr[i]>=0:
+            dp[i][j-arr[i]] += dp[i-1][j]
+        if j+arr[i]<=20:
+            dp[i][j+arr[i]] += dp[i-1][j]
+
+print(dp[-1][arr[-1]])

84b882bab5fcd0637698f6d1bdca82cc1604c9ec
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-03-14 23:49:54 +0900

    [Gold V] Title: 빗물, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -4,20 +4,20 @@ input = sys.stdin.readline
 h, w = map(int, input().split())
 height = list(map(int, input().split()))
 
+stack = []
 volume = 0
-left, right = 0, w-1
-left_max, right_max = height[left], height[right]
 
-while left < right:
-    left_max = max(height[left], left_max) 
-    right_max = max(height[right], right_max)
-    
-    if left_max <= right_max:
-        volume += left_max - height[left]
-        left += 1
-        
-    else:
-        volume += right_max - height[right]
-        right -= 1
+for i in range(w):
+    while stack and height[i] > height[stack[-1]]:
+        top = stack.pop()
 
+        if not len(stack):
+            break
+
+        distance = i - stack[-1] - 1
+        waters = min(height[i], height[stack[-1]]) - height[top]
+
+        volume += distance * waters
+
+    stack.append(i)
 print(volume)

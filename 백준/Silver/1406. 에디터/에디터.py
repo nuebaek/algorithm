5fba2ccab9c8f89ac7e7c93cf5c756ede76c6726
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-03-15 18:47:33 +0900

    [Silver II] Title: 에디터, Time: 228 ms, Memory: 42832 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+left_stack = list(input().strip())
+right_stack = []
+m = int(input())
+
+for _ in range(m):
+    command = input().split()
+
+    if command[0] == "L":
+        if left_stack: 
+            right_stack.append(left_stack.pop())
+
+    elif command[0] == "D":
+        if right_stack:  
+            left_stack.append(right_stack.pop())
+
+    elif command[0] == "B":
+        if left_stack:
+            left_stack.pop()
+
+    elif command[0] == "P":
+        left_stack.append(command[1])
+
+print("".join(left_stack + right_stack[::-1]))

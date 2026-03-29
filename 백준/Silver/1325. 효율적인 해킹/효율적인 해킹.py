ed609b191043312c6e3bb7f2656b9acdd8f85b9a
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-03-29 22:48:06 +0900

    [Silver I] Title: 효율적인 해킹, Time: 14640 ms, Memory: 211256 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,42 +1,44 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [[] for _ in range(n+1)]
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[b].append(a)
+def solve():
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+    
+    graph = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[b].append(a)
 
-def bfs(start, n, graph):
-    visited = [False] * (n + 1)
-    q = deque([start])
-    visited[start] = True
-    cnt = 1
+    def bfs(start):
+        q = deque([start])
+        visited = [False] * (n + 1)
+        visited[start] = True
+        cnt = 1
+        while q:
+            curr = q.popleft()
+            for neighbor in graph[curr]:
+                if not visited[neighbor]:
+                    visited[neighbor] = True
+                    cnt += 1
+                    q.append(neighbor)
+        return cnt
 
-    while q:
-        curr = q.popleft()
-        for neighbor in graph[curr]:
-            if not visited[neighbor]:
-                visited[neighbor] = True
-                q.append(neighbor)
-                cnt += 1
+    max_cnt = 0
+    ans = []
 
-    return cnt
+    for i in range(1, n + 1):
+        if not graph[i]:
+            curr_cnt = 1
+        else:
+            curr_cnt = bfs(i)
+        
+        if curr_cnt > max_cnt:
+            max_cnt = curr_cnt
+            ans = [i]
+        elif curr_cnt == max_cnt:
+            ans.append(i)
 
-max_cnt = 0
-ans = []
+    print(*(ans))
 
-for i in range(1, n + 1):
-    if not graph[i]:
-        curr_cnt = 1
-    else:
-        curr_cnt = bfs(i, n, graph)
-
-    if curr_cnt > max_cnt:
-        max_cnt = curr_cnt
-        ans = [i]
-    elif curr_cnt == max_cnt:
-        ans.append(i)
-
-print(*(ans))
+solve()

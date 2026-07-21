e967880cc4138a9bc613b55e070bf7b4a80abdcf
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-07-21 19:18:04 +0900

    [level 1] Title: 모의고사, Time: 1.66 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,14 +1,27 @@
 def solution(answers):
-    p1 = [1, 2, 3, 4, 5]
-    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    patterns = [p1, p2, p3]
-
-    scores = [0, 0, 0]
-    for i, ans in enumerate(answers):
-        for idx, pattern in enumerate(patterns):
-            if ans == pattern[i % len(pattern)]:
-                scores[idx] += 1
-
-    best = max(scores)
-    return [i + 1 for i, s in enumerate(scores) if s == best]
+    user1 = [1, 2, 3, 4, 5]
+    user2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    user3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    users = [user1, user2, user3]
+    
+    results = []
+    max_num = 0
+    
+    for i in range(3):
+        q = len(answers) // len(users[i])
+        r = len(answers) % len(users[i])
+        if q == 0:
+            users[i] = users[i][:r]
+            
+        users[i] = users[i] * q + users[i][:r]
+        result = [a - b for a, b in zip(users[i], answers)]
+        
+        if max_num <= result.count(0):
+            if max_num == result.count(0):
+                results.append(i+1)
+            else:
+                max_num = result.count(0)
+                results = [i+1]
+            
+    return results
+    

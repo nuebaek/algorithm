f8d5976c2342149c8080de9980a12cd3ebb66b7e
Daeun Baek <70556627+nuebaek@users.noreply.github.com>
2026-09-26 22:21:58 +0900

    [level 2] Title: 귤 고르기, Time: 26.66 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,18 +1,28 @@
 def solution(k, tangerine):
-    sizes = sorted(tangerine)
+    tangerine.sort()
     counts = []
     previous = None
+    types = 0
+    answer = 0
 
-    for size in sizes:
-        if size == previous:
+    for t in tangerine:
+        if t == previous:
             counts[-1] += 1
         else:
+            previous = t
             counts.append(1)
-        previous = size
-
+    
     counts.sort(reverse=True)
+    i = 0
+    
+    while types < k:
+        types += counts[i]
+        answer += 1
+        i += 1
+        
+    return answer
+    
 
-    for kinds, count in enumerate(counts, 1):
-        k -= count
-        if k <= 0:
-            return kinds
+# 1. 사이즈 별로 개수를 count
+# 2. 개수(counts)를 내림차순 정렬
+# 3. 개수(counts)를 순차적으로 더하면서, 현재 개수가 k와 같거나 크면 종류에 값을 더하는 것을 멈춘다
